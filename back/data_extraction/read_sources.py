1357aa6a138b9dae904c5596a5f29c224606ed0e
paolagc <estarguars113@gmail.com>
2018-10-29 17:59:55 -0500

    to-do use of custom modules in external scripts

--- back/data_extraction/read_sources.py
+++ back/data_extraction/read_sources.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+#custom models
+from data_management.models import CountryModel, Migration
+
 def read_csv_source(file_path, cols=[], filters={}, null_values={}):
     df = pd.read_csv(file_path, na_values=null_values)
     # apply column single value based filters
@@ -14,6 +17,10 @@ def read_csv_source(file_path, cols=[], filters={}, null_values={}):
     return df
 
 
+def get_country(short_name):
+    return CountryModel.query.filter_by(short_name=short_name).first()
+
+
 if __name__ == "__main__":
     base_file = './sources/general_migration0016.csv'
     
@@ -21,38 +28,9 @@ if __name__ == "__main__":
     filters = {}
     filters['Variable'] = 'Inflows of foreign population by nationality'
     cols = [
+        'CO2',
         'Country of birth/nationality',
-        'Country',
-        'Year',
-        'Value'
-    ]
-    null_values = {'Value': 0}
-    general_input_df = read_csv_source(base_file, cols, filters, null_values)
-
-    # type casting
-    general_input_df['Value'] = general_input_df['Value'] = general_input_df['Value'].fillna(0.0).astype(int)
-    
-
-    # general input migration
-    filters = {}
-    filters['Variable'] = 'Inflows of foreign population by nationality'
-    cols = [
-        'Country of birth/nationality',
-        'Country',
-        'Year',
-        'Value'
-    ]
-    null_values = {'Value': 0}
-    general_input_df = read_csv_source(base_file, cols, filters, null_values)
-
-    # type casting
-    general_input_df['Value'] = general_input_df['Value'] = general_input_df['Value'].fillna(0.0).astype(int)
-
-    # general input migration
-    filters = {}
-    filters['Variable'] = 'Inflows of foreign population by nationality'
-    cols = [
-        'Country of birth/nationality',
+        'COU',
         'Country',
         'Year',
         'Value'
@@ -62,11 +40,36 @@ if __name__ == "__main__":
 
     # type casting
     general_input_df['Value'] = general_input_df['Value'].fillna(0.0).astype(int)
-    
 
-    # general input migration
+    # text cleaning
+    text_cols = [
+        'CO2',
+        'Country of birth/nationality',
+        'COU',
+        'Country'
+    ]
+    for name in text_cols:
+        general_input_df[name] = general_input_df[name].str.strip().lower()
+
+
+    # iterate and save in db
+    for i, row in general_input_df.iterrows():
+        # first check if the countries already exists
+
+        # source
+        source_country = get_country(row['Country of birth/nationality'])
+        if(not source_country):
+            source_country = CountryModel(row['CO2'], row['Country of birth/nationality'])
+
+        # destination
+        dest_country = get_country(row['Country of birth/nationality'])
+        if(not dest_country):
+            dest_country = CountryModel(row['COU'], row['Country'])
+   
+    '''
+    # general output migration
     filters = {}
-    filters['Variable'] = 'Outflows of foreign population by nationality'
+    filters['Variable'] = 'Inflows of foreign population by nationality'
     cols = [
         'Country of birth/nationality',
         'Country',
@@ -74,10 +77,9 @@ if __name__ == "__main__":
         'Value'
     ]
     null_values = {'Value': 0}
-    general_output_df = read_csv_source(base_file, cols, filters, null_values)
+    general_outut_df = read_csv_source(base_file, cols, filters, null_values)
 
     # type casting
-    general_output_df['Value'] = general_output_df['Value'].fillna(0.0).astype(int)
+    general_outut_df['Value'] = general_outut_df['Value'].fillna(0.0).astype(int)
 
-    print(general_input_df.head())
-    print(general_output_df.head())
+    '''
--- back/data_management/app.py
+++ back/data_management/app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-
 app = Flask(__name__)
 app.debug = True
 
@@ -11,6 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://paolagc:passwd_1@localhost
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db.init_app(app)
+
 
 @app.route('/')
 def hello():
